0bc2ffc85b5d3b2bf4c7a3e5b74253e30f7eb0d2
NewJKH <newsun0805@gmail.com>
2025-07-06 02:32:23 +0900

    init

---
+++ guidance-core/src/main/java/org/nano/guidance/Guidance.java
@@ -0,0 +1,31 @@
+package org.nano.guidance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.nano.guidance.infra.GuidanceCommand;
+
+import java.util.Objects;
+
+public final class Guidance extends JavaPlugin {
+    /*
+        길라잡이 기능
+
+        command
+
+        entity
+
+        ...?
+
+
+     */
+
+    @Override
+    public void onEnable() {
+        Objects.requireNonNull(getCommand("길라잡이")).setExecutor(new GuidanceCommand());
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ guidance-test/src/test/java/org/nano/guidance/Test.java
@@ -0,0 +1,14 @@
+package org.nano.guidance;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+public class Test {
+
+    @BeforeEach
+    @DisplayName(" 테스트용 셋업 ")
+    public void setup(){
+
+    }
+
+}
